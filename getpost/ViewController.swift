43ea5efb91f55f3e028ef3c716936c77a8f0abcf
IwaoTakafumi <koshiiwao2904@icloud.com>
2019-01-20 18:53:23 +0900

    Initial Commit

---
+++ getpost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getpost
+//
+//  Created by 岩男高史 on 2019/01/20.
+//  Copyright © 2019 岩男高史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
